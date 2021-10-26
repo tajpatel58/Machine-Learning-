9144d0a3a5a5fa5430b599eeb34251771dcc24f8
tajpatel58 <tajpatel58@gmail.com>
2021-10-26 22:43:16 +0100

    Cleaning up Code

--- AiCore/Web Scraping/ArgosShoppingBot.py
+++ AiCore/Web Scraping/ArgosShoppingBot.py
@@ -22,28 +22,6 @@ cookies_button.click()
 
 sleep(1)
 
-"""
-
-account_button = driver.find_element_by_xpath('//*[@id="haas-v2"]/div[2]/div/div[4]/a[2]/span')
-account_button.click()
-
-sleep(7)
-
-email = driver.find_element_by_xpath('//*[@id="email-address"]')
-email.send_keys('tajpatel58@gmail.com')
-
-sleep(3)
-
-password = driver.find_element_by_xpath('//*[@id="current-password"]')
-password.send_keys('Taj290898')
-
-sleep(3)
-
-sign_in = driver.find_element_by_xpath('//*[@id="main"]/div/div/div/div/div/section/div[2]/div[1]/div/div/div[2]/form/button')
-sign_in.click()
-
-"""
-
 search = driver.find_element_by_id("searchTerm")
 search.send_keys(cat_num)
 search.send_keys(Keys.RETURN)
@@ -66,18 +44,6 @@ sleep(4)
 
 select_staples_corner = driver.find_element_by_xpath('//*[@id="content"]/main/div[1]/div[3]/div[1]/section[2]/section/div[9]/div[2]/div/div/div[1]/div/div[2]/div[3]/div/div/ol/li[1]/div/div[2]/button/span/span')
 select_staples_corner.click()
-
-"""
-select_store = driver.find_element_by_xpath('//*[@id="content"]/main/div[1]/div[3]/div[1]/section[2]/section/div[9]/div[2]/div[2]/div[5]/button')
-select_store.click()
-
-sleep(3)
-"""
-"""
-add_to_trolley = driver.find_element_by_xpath('//*[@id="content"]/main/div[1]/div[3]/div[1]/section[2]/section/div[9]/div[2]/div/div[7]/button/span/span')
-add_to_trolley.click()
-
-"""
 
 sleep(2)
 
@@ -111,17 +77,15 @@ discount_box = driver.find_element_by_xpath('//*[@id="promoReference"]')
 discount_box.send_keys('DISCOUNT')
 discount_box.send_keys(Keys.RETURN)
 
-sleep(6)
+sleep(5)
 
-payment_button = driver.find_element_by_xpath('//*[@id="app-content"]/main/div/div/div[2]/div[3]/div[3]/form/button')
-payment_button.click()
+payment_options = driver.find_element_by_xpath('//*[@id="app-content"]/main/div/div/div[2]/div[3]/div[4]/form/button')
 
-sleep(8)
-
-payment_options = driver.find_element_by_xpath('//*[@id="app-content"]/main/div/div/div[2]/div[3]/div[3]/div[2]')
 payment_options.click()
 
-cvc = driver.find_element_by_xpath('//*[@id="hps-cvv"]//div')
+sleep(20)
+
+cvc = driver.find_element_by_xpath('//*[@class="hps-detail hps-mandatory"]')
 
 sleep(3)
 
@@ -133,3 +97,5 @@ pay_button.click()
 
 # %%
 
+
+# %%
