975b829c6d67809520229a0ff9fe6ac8bcf0c010
tajpatel58 <tajpatel58@gmail.com>
2021-12-06 14:18:09 +0000

    Fixing Error in Prediction Matrix

--- Digit Classification/Functions_For_Model/predict.py
+++ Digit Classification/Functions_For_Model/predict.py
@@ -5,8 +5,5 @@ from .feed_forward import feed_forward
 # algorithm. We add one to the returned due to the indexxing in python. 
 def predict(X, params, input_layer, hidden_layer, output_layer):
     probability_mat = feed_forward(X, params, input_layer, hidden_layer, output_layer)
-    predicted_value =  np.argmax(probability_mat, axis=1) + 1 
-    if predicted_value == 10:
-        return 0 
-    else:
-        return predicted_value
+    predicted_values =  np.argmax(probability_mat, axis=1) + 1 
+    return predicted_values
