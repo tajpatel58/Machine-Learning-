0a4b7ca0c304570409264e4cad478adfa4f050e7
tajpatel58 <tajpatel58@gmail.com>
2021-12-13 22:39:03 +0000

    Passed Swap Nodes in Pairs

--- LeetCode/Swap Nodes in Pairs.py
+++ LeetCode/Swap Nodes in Pairs.py
@@ -12,35 +12,50 @@ class ListNode:
 
 
 def swap_nodes(l1):
-    if l1 == []:
-        return []
+    if l1 == None:
+        return None
     elif l1.next == None:
         return l1 
     else:
-        head = l1.next.val
+        head = ListNode(l1.next.val)
         head.next = new_list = ListNode(l1.val, next=None)
         try:
             first_node = l1.next.next
-            second_node = l1.next.next.next
             first_val = first_node.val
+            odd = 1 
+            second_node = l1.next.next.next
             second_val = second_node.val
+            odd = 0 
+            new_list.next = ListNode(second_val, next=ListNode(first_val, next=None))
+            new_list = new_list.next.next
             while second_node.next != None: 
-                new_list.next = ListNode(second_val, next=ListNode(first_val, next=None))
                 first_node = first_node.next.next
-                second_node = second_node.next.next
                 first_val = first_node.val
+                odd = 1 
+                second_node = second_node.next.next
                 second_val = second_node.val
+                odd = 0
+                new_list.next = ListNode(second_val, next=ListNode(first_val, next=None))
+                new_list = new_list.next.next
+        except:
+            pass
+        if odd == 1:
+            try:
+                new_list.next = ListNode(first_val, next=None)
+            except:
+                pass
 
         return head
 
-
-node_4 = ListNode(4)
+node_2_3 = ListNode(2)
+node_2_2 = ListNode(2, next=node_2_3)
+node_6 = ListNode(6, next=node_2_2)
+node_4 = ListNode(4, next=node_6)
 node_3 = ListNode(val=3, next=node_4)
-node_2 = ListNode(val=2, next=node_3)
-easy_list = ListNode(1, next=node_2)
+node_5 = ListNode(val=5, next=node_3)
+easy_list = ListNode(2, next=node_5)
 
 swapped = swap_nodes(easy_list)
-# print(swapped.next.next)
 
     
     
